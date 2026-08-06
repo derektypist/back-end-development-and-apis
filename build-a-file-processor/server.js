a6148f8902bc52d61e58b69bb38364da32f7f59a
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-06 15:43:58 +0000

    Applied alloc method to buffer

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -3,3 +3,5 @@ const buf = Buffer.from('Hello, Node!');
 console.log(buf);
 console.log(buf.toString("hex"));
 console.log(buf.toString("base64"));
+const buf2 = Buffer.alloc(8, 0xff);
+console.log(buf2);
