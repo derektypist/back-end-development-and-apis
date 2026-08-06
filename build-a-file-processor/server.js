d7595239c16c55bd3d51033809db5f2ed2e3be0b
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-06 15:46:38 +0000

    added decoded

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -5,3 +5,5 @@ console.log(buf.toString("hex"));
 console.log(buf.toString("base64"));
 const buf2 = Buffer.alloc(8, 0xff);
 console.log(buf2);
+const decoded = Buffer.from('ZnJlZUNvZGVDYW1w','base64').toString('utf8');
+console.log(decoded);
