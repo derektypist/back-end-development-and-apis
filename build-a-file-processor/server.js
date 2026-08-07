821d20dba1849f8075f7085212b958065a52823d
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 08:30:31 +0000

    started crypto

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,9 +1,4 @@
 // Starter file — add your code here
-const buf = Buffer.from('Hello, Node!');
-console.log(buf);
-console.log(buf.toString("hex"));
-console.log(buf.toString("base64"));
-const buf2 = Buffer.alloc(8, 0xff);
-console.log(buf2);
-const decoded = Buffer.from('ZnJlZUNvZGVDYW1w','base64').toString('utf8');
-console.log(decoded);
+const crypto = require("crypto");
+const hash = crypto.createHash("sha256").update("freeCodeCamp").digest("hex");
+console.log(hash);
