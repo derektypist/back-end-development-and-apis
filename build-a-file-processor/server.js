75a1c3ecc135ce67ff92de663ba7c9dba9dd008e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 09:15:57 +0000

    applied createWriteStream

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,10 +1,6 @@
 // Starter file — add your code here
 const fs = require("fs");
-const readable = fs.createReadStream("assets/poem.txt", {encoding: "utf8"});
-readable.on("data", (chunk) => {
-    console.log(chunk);
-});
-
-readable.on("end", () => {
-    console.log("Done reading");
-});
+const writable = fs.createWriteStream("assets/stream-output.txt");
+writable.write("First chunk\n");
+writable.write("Second chunk\n");
+writable.end();
