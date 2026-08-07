3ff47213a51660516809f9a3b1519b5d78852082
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 08:52:32 +0000

    added methods to path

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -2,3 +2,6 @@
 const path = require("path");
 const fullPath = path.join(__dirname, "assets", "poem.txt");
 console.log(fullPath);
+console.log(path.basename(fullPath));
+console.log(path.dirname(fullPath));
+console.log(path.extname(fullPath));
