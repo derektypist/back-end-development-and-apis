fe113ec49804e524c90e899bacf9e5e944bcb4a4
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 08:55:12 +0000

    added join and resolve

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -5,3 +5,5 @@ console.log(fullPath);
 console.log(path.basename(fullPath));
 console.log(path.dirname(fullPath));
 console.log(path.extname(fullPath));
+console.log(path.join("assets", "..", "server.js"));
+console.log(path.resolve("assets", "..", "server.js"));
