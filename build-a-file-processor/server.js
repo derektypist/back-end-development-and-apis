eaa1b32cd8bb64ded1fd755e30df7ed1c1cfdf3f
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 08:39:10 +0000

    added randomUUID

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,4 +1,4 @@
 // Starter file — add your code here
 const crypto = require("crypto");
-const random = crypto.randomBytes(16).toString("hex");
-console.log(random);
+const id = crypto.randomUUID();
+console.log(id);
