d2f43b2dcac953f8c2ee111925e780aad206e777
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-07 08:45:41 +0000

    added totalmem, freemem and uptime

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -3,3 +3,6 @@ const os = require("os");
 console.log(os.platform());
 console.log(os.arch());
 console.log(os.hostname());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.uptime());
