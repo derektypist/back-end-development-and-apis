259dd887b9de65ac6d99aeaea4b50db569b71b68
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-10 10:39:37 +0000

    updated index.test.js

--- build-a-case-converter/case_converter/index.test.js
+++ build-a-case-converter/case_converter/index.test.js
@@ -4,4 +4,4 @@ const caseConverter = require("./index");
 assert.strictEqual(caseConverter.getUpperCase("hello free Code Camp!"),"HELLO FREE CODE CAMP!");
 assert.strictEqual(caseConverter.getLowerCase("hello free Code Camp!"),"hello free code camp!");
 assert.strictEqual(caseConverter.getProperCase("hello free Code Camp!"),"HeLlo free Code Camp!");
-assert.strictEqual(caseConverter.getSentenceCase("hello free Code Camp!"),"Hello fRee code camp!");
+assert.strictEqual(caseConverter.getSentenceCase("hello free Code Camp!"),"Hello free code camp!");
