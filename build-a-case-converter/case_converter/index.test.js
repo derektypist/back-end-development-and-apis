1f9c78649a2f67ce6a3594156c244538af2b148e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-10 10:29:02 +0000

    Added caseConveter at index.test.js

--- build-a-case-converter/case_converter/index.test.js
+++ build-a-case-converter/case_converter/index.test.js
@@ -1,3 +1,4 @@
 const assert = require('node:assert/strict');
+const caseConverter = require("./index");
 
 assert.strictEqual(caseConverter.getUpperCase("hello free Code Camp!"),"HELLO FREE CODE CAMP!");
