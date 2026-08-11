856089042ffc50e860de49f45cb59fb8d15af075
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:58:10 +0000

    added response.end

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -5,9 +5,8 @@ const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
   const url = request.url === "/" ? "/index.html" : request.url;
-  
   const filePath = join("public", url);
-  response.end(filePath);
+  response.end(filePath, "utf-8");
 });
 
 server.listen(3001);
