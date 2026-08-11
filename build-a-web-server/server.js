a35b219e883191cd3907cfacf49ee2f0d63b4156
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:54:49 +0000

    added filePath variable

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -3,8 +3,10 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
+  const { join } = require("path");
   const url = request.url === "/" ? "/index.html" : request.url;
   response.end(url, "utf-8");
+  const filePath = join("public", url);
 });
 
 server.listen(3001);
