fe7b30c54c1107e9537cdd1d4b4cc655c8154257
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 10:22:15 +0000

    lesson 32

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -7,13 +7,13 @@ const server = http.createServer((request, response) => {
   console.log(request.url);
   const url = request.url === "/" ? "/index.html" : request.url;
   const filePath = join("public", url);
-  response.end(filePath, "utf-8");
 
   readFile(filePath, (error, file) => {
     if (error) {
-        console.error(error);
-        return;
+      console.error(error);
+      return;
     }
+    response.end(file, "utf-8");
   });
 });
 
