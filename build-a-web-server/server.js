f9674d3de26e1eada4fbcc38956dfcbf0bac1db8
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:49:58 +0000

    added variable url

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -4,6 +4,7 @@ const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
   response.end(request.url);
+  let url = request.url === "/" ? "/index.html" : request.url;
 });
 
 server.listen(3001);
