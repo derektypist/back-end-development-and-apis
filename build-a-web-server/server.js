af88cd07bce2f0e372d41db6ff72951cda60ac16
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:34:20 +0000

    added request

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {});
+const server = http.createServer((request, response) => {
+    console.log(request);
+});
 
 server.listen(3001);
