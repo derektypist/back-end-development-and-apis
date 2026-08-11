4587f54190b5847fccad761fdf55d70e6061af17
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:57:11 +0000

    added filepath

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -4,10 +4,10 @@ const { join } = require("path");
 const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
-  const { join } = require("path");
   const url = request.url === "/" ? "/index.html" : request.url;
-  response.end(url, "utf-8");
+  
   const filePath = join("public", url);
+  response.end(filePath);
 });
 
 server.listen(3001);
