ab3f4d2dab3230fcd5606a6804777794cf7e5270
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:51:16 +0000

    updated server

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -3,8 +3,8 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
-  response.end(request.url);
-  let url = request.url === "/" ? "/index.html" : request.url;
+  const url = request.url === "/" ? "/index.html" : request.url;
+  response.end(url, "utf-8");
 });
 
 server.listen(3001);
