98ae4188e927cd0be100932a457c4754e2ea9c46
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-08-11 09:43:55 +0000

    added response.end

--- build-a-web-server/server.js
+++ build-a-web-server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   console.log(request.headers);
   console.log(request.url);
+  response.end("Data to send back to the client");
 });
 
 server.listen(3001);
